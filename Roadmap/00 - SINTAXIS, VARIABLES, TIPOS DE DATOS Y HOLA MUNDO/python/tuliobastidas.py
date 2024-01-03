33100da07b9eba8b6d572546d68b8a44bc9c227d
tuliobast <108601755+tuliobast@users.noreply.github.com>
2024-01-03 13:08:11 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/tuliobastidas.py
@@ -0,0 +1,14 @@
+# https://www.python.org/
+""" 
+esta es otra forma de agregar comentarios
+con varias lineas en python
+"""
+# variables primarias
+PI = 3.14169 
+boleano1 = True
+boleano2 = False
+entero = 1
+flotante = 1.0
+cadena = 'Esta es una cadena de texto'
+
+print('Hola, Python')
